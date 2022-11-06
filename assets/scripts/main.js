f607aa2aebdf652eb4fc900968c58403092cc05d
l2lee <l2lee@ucsd.edu>
2022-11-06 13:05:49 -0800

    Explore done

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,9 @@ function getRecipesFromStorage() {
   // A9. TODO - Complete the functionality as described in this function
   //           header. It is possible in only a single line, but should
   //           be no more than a few lines.
+  const recipes = localStorage.getItem('recipes');
+
+  return JSON.parse(recipes);
 }
 
 /**
@@ -35,10 +38,18 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
   // A10. TODO - Get a reference to the <main> element
+  const main = document.querySelector('main');
   // A11. TODO - Loop through each of the recipes in the passed in array,
   //            create a <recipe-card> element for each one, and populate
   //            each <recipe-card> with that recipe data using element.data = ...
   //            Append each element to <main>
+  console.log(recipes);
+
+  for (const recipe of recipes) {
+    const recipeCard = document.createElement('recipe-card');
+    recipeCard.data = recipe;
+    main.appendChild(recipeCard);
+  }
 }
 
 /**
@@ -60,7 +71,7 @@ function saveRecipesToStorage(recipes) {
 function initFormHandler() {
 
   // B2. TODO - Get a reference to the <form> element
-  
+
   // B3. TODO - Add an event listener for the 'submit' event, which fires when the
   //            submit button is clicked
 
@@ -77,7 +88,7 @@ function initFormHandler() {
 
   // B10. TODO - Get a reference to the "Clear Local Storage" button
   // B11. TODO - Add a click event listener to clear local storage button
-  
+
   // Steps B12 & B13 will occur inside the event listener from step B11
   // B12. TODO - Clear the local storage
   // B13. TODO - Delete the contents of <main>
